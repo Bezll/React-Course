db9fab497b2c6c410e60d28309eac9bc79030b53
Bezll <jabez1992@hotmail.com>
2020-06-01 20:44:34 +0100

    stateless functional comps

---
+++ indecision-app/src/playground/stateless-functional-components.js
@@ -0,0 +1,10 @@
+const User = (props) => {
+  return (
+    <div>
+      <p>Name: {props.name}</p>
+      <p>Age: {props.age}</p>
+    </div>
+  );
+};
+
+ReactDOM.render(<User name="Andrew" age={26} />, document.getElementById('app'));
